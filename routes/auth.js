4843a56467fed6da5d16389e59075f632f6093ce
aglyvin <alexej@glyvin.com>
2022-09-04 13:25:01 +0300

    add custom validator

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,19 @@ const {
   login,
 } = require('../controllers/users');
 
+function validateAvatar(url) {
+  const regex = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/g;
+  if (regex.test(url)) {
+    return url;
+  }
+  throw new Error('Невалидная ссылка на аватар');
+}
+
 router.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validateAvatar),
   }),
 }), createUser);
 router.post('/signin', login);
