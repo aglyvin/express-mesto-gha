619040d53acdb52249a7866fa06f77d05ba2fb48
aglyvin <alexej@glyvin.com>
2022-09-04 13:08:38 +0300

    Add Joi to auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   createUser,
   login,
 } = require('../controllers/users');
 
-router.post('/signup', createUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
 router.post('/signin', login);
 
 module.exports = router;
