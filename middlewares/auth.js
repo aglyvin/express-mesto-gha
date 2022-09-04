bd0c321970ae3d0a1949f16c4eed03f77245961c
aglyvin <alexej@glyvin.com>
2022-09-04 17:12:16 +0300

    changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 const { ForbiddenError } = require('../errors/ForbiddenError');
+const { UnauthorizeError } = require('../errors/UnauthorizeError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new ForbiddenError('Необходима авторизация');
+    throw new UnauthorizeError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new ForbiddenError('Необходима авторизация');
+    throw new UnauthorizeError('Необходима авторизация');
   }
   req.user = payload;
   next();
